0e9397a085662894dfd0949b5fc47fd18da23fa7
CGMcBride <connermcbride@ymail.com>
2020-05-14 10:58:08 -0500

    changed all sheets, added time an storeage

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-const events = [
-	{ start: 30, end: 150 },
-	{ start: 540, end: 600 },
-	{ start: 560, end: 620 },
-	{ start: 610, end: 670 },
-];
-layOutDay(events);
+//alert("test");
+//logic to style elements here
+
+//logic to render local storage
+$("#9 .description").val(localStorage.getItem("9"));
+$("#10 .description").val(localStorage.getItem("10"));
+$("#11 .description").val(localStorage.getItem("11"));
+$("#12 .description").val(localStorage.getItem("12"));
+$("#1 .description");
+// on click events are at the bottom
+$(".saveBtn").click(function () {
+	let textValue = $(this).siblings(".description").val();
+	// console.log(timeOne);
+	let timeKey = $(this).parent().attr("id");
+	// console.log(timeTwo);
+	localStorage.setItem(timeKey, textValue);
+});
+$("#currentDay").text(moment().format("MMMM/Do/YYYY"));
